95d6b34c240d42116972a282ae5fbccacc29735a
auooru <auooru@outlook.com>
2024-06-13 22:55:05 +0800

    optimize: axios 扩展配置使用更加灵活的 extractLevel 替代 extractData

--- packages/axios/src/axios.d.ts
+++ packages/axios/src/axios.d.ts
@@ -5,7 +5,7 @@ declare module 'axios' {
     // 是否启用鉴权流程
     authorization?: boolean;
     // 是否直接导出响应数据
-    extractData?: boolean;
+    extractLevel?: 0 | 1 | 2;
     // 是否静默错误通知
     silentErrorNotify?: boolean;
   }
