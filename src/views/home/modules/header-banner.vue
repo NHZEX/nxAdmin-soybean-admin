5f35df81966cc156ce5cd13666cdcd3a493754a5
auooru <auooru@outlook.com>
2024-06-19 17:49:16 +0800

    chore: 移除首页中暂时用不到的组件

--- src/views/home/modules/header-banner.vue
+++ src/views/home/modules/header-banner.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { computed } from 'vue';
-import { $t } from '@/locales';
 import { useAppStore } from '@/store/modules/app';
 import { useAuthStore } from '@/store/modules/auth';
 
@@ -20,21 +19,11 @@ interface StatisticData {
 }
 
 const statisticData = computed<StatisticData[]>(() => [
-  {
-    id: 0,
-    label: $t('page.home.projectCount'),
-    value: '25'
-  },
-  {
-    id: 1,
-    label: $t('page.home.todo'),
-    value: '4/16'
-  },
-  {
-    id: 2,
-    label: $t('page.home.message'),
-    value: '12'
-  }
+  // {
+  //   id: 0,
+  //   label: $t('page.home.projectCount'),
+  //   value: '25'
+  // },
 ]);
 </script>
 
@@ -43,14 +32,11 @@ const statisticData = computed<StatisticData[]>(() => [
     <NGrid :x-gap="gap" :y-gap="16" responsive="screen" item-responsive>
       <NGi span="24 s:24 m:18">
         <div class="flex-y-center">
-          <div class="size-72px shrink-0 overflow-hidden rd-1/2">
-            <img src="@/assets/imgs/soybean.jpg" class="size-full" />
-          </div>
           <div class="pl-12px">
             <h3 class="text-18px font-semibold">
-              {{ $t('page.home.greeting', { userName: authStore.userInfo?.username }) }}
+              {{ authStore.userInfo?.username }}
             </h3>
-            <p class="text-#999 leading-30px">{{ $t('page.home.weatherDesc') }}</p>
+            <p class="text-#999 leading-30px">欢迎访问</p>
           </div>
         </div>
       </NGi>
